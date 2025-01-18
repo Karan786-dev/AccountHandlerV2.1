3faefd07259541eb77a325e69890001d9b769db4
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2025-01-18 12:36:48 +0000

    Refactor messageHandler to use asyncio for concurrent task execution

--- syncerBotHandler.py
+++ syncerBotHandler.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 from pyrogram.raw.types import UpdateGroupCall , GroupCallDiscarded
 from database import Channels , Accounts
 from orderAccounts import UserbotManager
-import random
+import asyncio
 
 async def messageHandler(_,message:Message):
     channelID = message.chat.id 
@@ -16,29 +16,31 @@ async def messageHandler(_,message:Message):
     tasksData = channelData.get("services",[])
     if not len(tasksData):  return
     reactionEmojis = channelData.get('reactionsType', [])
+    tasksArray = []
     if ("view_posts" in tasksData) and channelData.get("isViewEnabled",False):
         viewRestTime = channelData.get("viewRestTime",0)
         viewCount = channelData.get("viewCount",0)
         userbots = list(Accounts.find({}))
-        await UserbotManager.bulk_order(userbots,{
+        tasksArray.append(UserbotManager.bulk_order(userbots,{
             "type":"viewPosts",
             "postLink": postLink,
             "restTime":viewRestTime,
             "taskPerformCount": int(viewCount),
             "inviteLink": inviteLink
-        })
+        }))
     if ("reaction_posts" in tasksData) and channelData.get("isReactionsEnabled",False) and len(reactionEmojis):
         reactRestTime = channelData.get('reactionRestTime',0)
         reactionCount = channelData.get('reactionsCount',0) 
         userbots = list(Accounts.find({}))
-        await UserbotManager.bulk_order(userbots,{
+        tasksArray(UserbotManager.bulk_order(userbots,{
             "type":"reactPost",
             "postLink": postLink,
             "restTime":float(reactRestTime),
             "taskPerformCount": int(reactionCount),
             "emoji":reactionEmojis,
             "inviteLink": inviteLink
-        })
+        }))
+    await asyncio.gather(*tasksArray)
     
         
 async def voiceChatHandler(client:Client, update, users, chats):
